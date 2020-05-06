f0bfc771c9e703c4f60835245d243e02bf79d6fb
Apilv <38555903+Apilv@users.noreply.github.com>
2020-05-06 19:27:44 +0300

    Design + Render list added

---
+++ js/data/todos.js
@@ -0,0 +1,22 @@
+"use strict"
+
+let todo_list = [
+    {
+        description: 'Pasigaminti arbatos',
+        created_on: '2020-05-06 11:01',
+        deadline: '2020-05-06 12:50',
+        status: 'todo'
+    },
+    {
+        description: 'Paklausyt paskaita',
+        created_on: '2020-05-06 11:02',
+        deadline: '2020-05-06 13:00',
+        status: 'done'
+    },
+    {
+        description: 'Spresti CodeWars',
+        created_on: '2020-05-05 09:00',
+        deadline: '2040-07-06 17:00',
+        status: 'in-progress'
+    }
+];
